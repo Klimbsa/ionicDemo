058f987684b4d089f325e490b582bb8fde0327b7
Klimbsa <marvin.82100@hotmail.fr>
2018-03-16 11:47:40 +0100

    second commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+   
+	heroes:any = [
+                    {
+                      "realname": "Bruce Wayne",
+                      "name": "Batman",
+                      "cover": "https://avatarfiles.alphacoders.com/442/44272.jpg",
+                      "editor": "DC",
+                      "movies": [
+                        {"title": "The Dark Knight Rises", "releaseDate":"2012-07-05"},
+                        {"title": "The Dark Knight", "releaseDate":"2008-08-13"},
+                        {"title": "Batman Begins", "releaseDate":"2005-06-15"}
+                      ]
+                    },
+                    {
+                      "realname": "Clark Kent",
+                      "name": "Superman",
+                      "cover": "DC",
+                      "cover": "http://classicmarvelforever.com/cms/assets/images/profiles/comic_book/Superman5.png",
+                      "movies": [
+                        {"title": "Batman v Superman", "releaseDate":"2016-07-05"},
+                        {"title": "Man of Steel", "releaseDate":"2013-08-13"}
+                      ]
+                    },
+                    {
+                      "realname": "Matt Murdock",
+                      "name": "Daredevil",
+                      "cover": "Marvel",
+                      "cover": "https://vignette.wikia.nocookie.net/p__/images/7/7b/IMG_6702.jpg/revision/latest?cb=20170912160050&path-prefix=protagonist",
+                      "movies": [
+                        {"title": "Daredevil : The Movie", "releaseDate":"2003-07-05"}
+                      ]
+                    }
+                  ]
 
-  }
+  	constructor(public navCtrl: NavController) {
+  	}
 
 }
